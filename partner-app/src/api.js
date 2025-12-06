28b3547ab5fe3e35e53b2d9b86f051e8d4119a99
vishalsinha2004 <vishalsinha6567@gmail.com>
2025-12-07 00:44:15 +0530

    add url

--- partner-app/src/api.js
+++ partner-app/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'https://indora.onrender.com' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('partnerToken')) {
